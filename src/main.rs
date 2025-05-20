13dd42b5b14426b286ac89937a5417214c3fa63e
bitbyte08 <eight2823@naver.com>
2025-05-20 15:49:25 +0900

    feat :: gtk4 test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use gtk4::prelude::*;
+use gtk4::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.example.HelloWorld")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        // Don't forget to make all widgets visible.
+        win.show();
+    });
+
+    app.run();
 }
